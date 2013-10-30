f15098f2f72d78915d80695d406decc0b924d75d
Gerardo Cobo <gera.cobo@gmail.com>
2013-10-30 11:59:43 -0600

    Muchos cambios...

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ def main():
 	"""
 
 	ekg_queue = multiprocessing.Queue()
+	temp_queue = multiprocessing.Queue()
 	spo2_queue = multiprocessing.Queue()
+	alarm_queue = multiprocessing.Queue()
 
 	print "Creando procesos de comunicación..."
-	st = multiprocessing.Process(target=server_process, args=(ekg_queue, spo2_queue))
-	dt = multiprocessing.Process(target=daq_process, args=(ekg_queue, spo2_queue))
+	st = multiprocessing.Process(target=server_process, args=(ekg_queue, temp_queue, spo2_queue, alarm_queue))
+	dt = multiprocessing.Process(target=daq_process, args=(ekg_queue, temp_queue, spo2_queue, alarm_queue))
 
 	st.daemon = True
 	dt.daemon = True
@@ -37,6 +39,10 @@ def main():
 	cmd = ""
 	while(cmd != "salir"):
 		cmd = raw_input()
+		if cmd == "alarm on":
+			alarm_queue.put(True)
+		if cmd == "alarm off":
+			alarm_queue.put(False)
 		parse_cmd(cmd)
 
 
@@ -49,7 +55,7 @@ def parse_cmd(cmd):
 		pass
 
 
-def server_process(ekg_queue, spo2_queue):
+def server_process(ekg_queue, temp_queue, spo2_queue, alarm_queue):
 	"""
 	This thread communicates with the server. When it finds data in the
 	queue, it creates a packet with the following form:
@@ -70,35 +76,37 @@ def server_process(ekg_queue, spo2_queue):
 		print "ERROR: No se pudo conectar con el servidor."
 		return
 
-
 	while True:
 		packet= {}
 		if not ekg_queue.empty():
 			packet['ekg'] = ekg_queue.get()
+		if not temp_queue.empty():
+			packet['temp'] = temp_queue.get()
 		if not spo2_queue.empty():
 			packet['spo2'] = spo2_queue.get()
+			print packet['spo2']
+
+
+		if not alarm_queue.empty():
+			alarm_state = alarm_queue.get()
+			print alarm_state
+			if alarm_state == True:
+				print "Prendiendo Alarma..."
+				packet['alarm'] = 'on'
+			else:
+				print "Apagando Alarma..."
+				packet['alarm'] = 'off'
 
 		packetJson = json.dumps(packet)
 
-		print "Mandando Paquete: ", packetJson
+		#print "Mandando Paquete: ", packetJson
 		server.send(str(packetJson)+"|")
 		time.sleep(.01)
 
-def daq_process(ekg_queue, spo2_queue):
+def daq_process(ekg_queue, temp_queue, spo2_queue, alarm_queue):
 	"""
 	This thread communicates with the daq.
 	"""
-
-	import random
-
-	#volts = 200
-	#delta = 0
-	#while True:
-	#	delta = random.randint(-5,5)
-	#	volts = volts + delta
-	#	ekg_queue.put(volts)
-	#	spo2_queue.put(5)
-
 	from u3 import U3
 	from math import log, fabs, log10
 
@@ -110,10 +118,30 @@ def daq_process(ekg_queue, spo2_queue):
 		print "ERROR: No se pude conectar con el daq."
 		return
 
+	alarm_state = False
 
 	while True:
+
+		# Read Alarm
+		al = daq.getFIOState(7)
+
+		# Check if there was a change in state
+		#if al != alarm_state:
+		#	alarm_queue.put(al)
+		#	alarm_state = al
+
+
+		if al:
+			alarm_queue.put(True)
+
+		# Read EKG
 		ekg = daq.getAIN(0)
 		ekg_queue.put([ekg*70])
+
+
+		# Read Temp
+		temp = daq.getAIN(2)
+		temp_queue.put([temp])
 
 		sleep = .005
 
@@ -129,14 +157,14 @@ def daq_process(ekg_queue, spo2_queue):
 		photo2 = daq.getAIN(1)
 		daq.setFIOState(6, state=0)
 
-		R = (log10(fabs(photo1)) / log10(fabs(photo2)))
+		R = log(fabs(photo1)) / log(fabs(photo2))
 
 		#spo2 = 108.611 - 20.1389*R - 3.47222*R**2
 		spo2 = R*100
 
 		spo2_queue.put([spo2])
 
-		#time.sleep(sleep)
+		time.sleep(sleep)
 
 
 
